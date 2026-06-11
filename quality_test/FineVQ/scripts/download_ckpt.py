c63fd2e74e526d4f65223ecf253681b863d4086c
wuxiuyu.wxy <xiuyuwu@tencent.com>
2026-06-11 23:21:09 +0800

    save work before filtering

--- quality_test/FineVQ/scripts/download_ckpt.py
+++ quality_test/FineVQ/scripts/download_ckpt.py
@@ -2,17 +2,17 @@ import os
 from huggingface_hub import snapshot_download
 
 # 1. 设置镜像站（国内必做，否则 InternVL 这种大模型基本下不动）
-os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
+# os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
 
 # 2. 执行下载
 # repo_id: 模型名字
 # local_dir: 你想存到的本地目录
 # resume_download: 断点续传，断网了重跑脚本就行
 try:
-    print("开始下载 InternVL2-8B，请稍候...")
+    print("开始下载 FineVQ_score，请稍候...")
     snapshot_download(
-        repo_id="OpenGVLab/InternVL2-8B",
-        local_dir="./ckpt",
+        repo_id="IntMeGroup/FineVQ_score",
+        local_dir="./ckpt/FineVQ_score",
         resume_download=True,
         max_workers=8  # 开启多线程下载
     )
